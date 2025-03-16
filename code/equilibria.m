4ea7f042b41d95ed56d7069e466912f3027dfd55
Jodok Dittmann <jodok@dittmann.at>
2025-03-16 15:51:02 +0100

    Added MPC based on OSQP example

--- code/equilibria.m
+++ code/equilibria.m
@@ -1,23 +1,21 @@
-function [delta_ss, ddeltadt_ss, omega_F_ss, omega_R_ss] = equilibria(V_ss, beta_ss, dpsidt_ss, p, x0)
+function [delta_ss, omega_F_ss, omega_R_ss] = equilibria(V_ss, beta_ss, dpsidt_ss, p, x0)
 
 xsol = fsolve(@(x) f(x, V_ss, beta_ss, dpsidt_ss, p), x0);
 
 delta_ss = xsol(7);
-ddeltadt_ss = xsol(8);
-omega_F_ss = xsol(9);
-omega_R_ss = xsol(10);
+omega_F_ss = xsol(8);
+omega_R_ss = xsol(9);
 
 function y = f(x, V_ss, beta_ss, dpsidt_ss, p)
     zeta(1) = x(1); % V
     zeta(2) = x(2); % beta
     zeta(3) = x(3); % psi
     zeta(4) = x(4); % dpsidt
-    zeta(5) = x(5); % delta
-    zeta(5) = x(6); % X
-    zeta(6) = x(7); % Y
-    u(1) = x(8); % ddeltadt
-    u(2) = x(9); % omega_F
-    u(3) = x(10); % omega_R
+    zeta(5) = x(5); % X
+    zeta(6) = x(6); % Y
+    u(1) = x(7); % ddeltadt
+    u(2) = x(8); % omega_F
+    u(3) = x(9); % omega_R
     dzetadt = dynamics(zeta, u, p);
     y(1) = x(1) - V_ss;
     y(2) = x(2) - beta_ss;
@@ -25,7 +23,6 @@ function y = f(x, V_ss, beta_ss, dpsidt_ss, p)
     y(4) = dzetadt(1); % dVdt
     y(5) = dzetadt(2); % dbetadt
     y(6) = dzetadt(4); % dpsiddt
-    y(7) = dzetadt(5); % ddeltadt
 end
 
 end
--- code/main.m
+++ code/main.m
@@ -6,50 +6,71 @@ R_ss = 7;
 beta_ss = -51*pi/180;
 dpsidt_ss = V_ss/R_ss;
 
-x0 = [V_ss, beta_ss, 0, dpsidt_ss, 0, 0, -40.7*pi/180, 0, 20.44, 58.33];
+% Equilibria
 
-[delta_ss, ddeltadt_ss, omega_F_ss, omega_R_ss] = equilibria(V_ss, beta_ss, dpsidt_ss, p, x0);
+x0 = [V_ss, beta_ss, 0, dpsidt_ss, 0, -40.7*pi/180, 0, 20.44, 58.33];
+[delta_ss, omega_F_ss, omega_R_ss] = equilibria(V_ss, beta_ss, dpsidt_ss, p, x0);
 
 m = 3;
-n = 7;
+n = 6;
+
+% Linearisation
 
 syms zeta [n 1]
 syms u [m 1]
 
 dzetadt = dynamics(zeta, u, p);
 
-A_c = jacobian(dzetadt([1, 2, 4, 5]), zeta([1, 2, 4, 5]));
-B_c = jacobian(dzetadt([1, 2, 4, 5]), u);
+A_c = jacobian(dzetadt([1, 2, 4]), zeta([1, 2, 4]));
+B_c = jacobian(dzetadt([1, 2, 4]), u);
 
-A_c = double(subs(A_c, {zeta1, zeta2, zeta4, zeta5, u1, u2, u3}, {V_ss, beta_ss, dpsidt_ss, delta_ss, ddeltadt_ss, omega_F_ss, omega_R_ss}));
-B_c = double(subs(B_c, {zeta1, zeta2, zeta4, zeta5, u1, u2, u3}, {V_ss, beta_ss, dpsidt_ss, delta_ss, ddeltadt_ss, omega_F_ss, omega_R_ss}));
+A_c = double(subs(A_c, {zeta1, zeta2, zeta4, u1, u2, u3}, {V_ss, beta_ss, dpsidt_ss, delta_ss, omega_F_ss, omega_R_ss}));
+B_c = double(subs(B_c, {zeta1, zeta2, zeta4, u1, u2, u3}, {V_ss, beta_ss, dpsidt_ss, delta_ss, omega_F_ss, omega_R_ss}));
 
-h = 1e-3;
+% Discretisation
+
+ts = 1e-3;
 tf = 30;
 
-[A_d, B_d] = c2d(A_c, B_c, h);
+[A_d, B_d] = c2d(A_c, B_c, ts);
 
-Q = eye(4);
-R = eye(3);
+% LQR
 
-[K, S, P] = dlqr(A_d, B_d, Q, R);
+l = 3;
 
-zeta0 = zeros(n, 1);  
-zeta0(1) = 7;
+Q = eye(l);
+R = eye(m);
 
-U = nan(m, tf/h + 1); 
-Zeta = nan(n, tf/h + 1); 
+K = dlqr(A_d, B_d, Q, R);
 
-Zeta(:, 1) = [V_ss; beta_ss; 0; dpsidt_ss; delta_ss; 0; 0] + rand(n, 1);
+% MPC
 
-for k = 1:tf/h
-    U(:, k) = [ddeltadt_ss; omega_F_ss; omega_R_ss] - K*(Zeta([1, 2, 4, 5], k) - [V_ss; beta_ss; dpsidt_ss; delta_ss]);
-    [t, zeta] = ode45(@(t, zeta) dynamics(zeta, U(:, k), p), [(k - 1)*h k*h], Zeta(:, k));
+N = 10;
+
+% Initialisation
+
+U = nan(m, tf/ts + 1); 
+Zeta = nan(n, tf/ts + 1); 
+Zeta(:, 1) = [V_ss; beta_ss; 0; dpsidt_ss; 0; 0] + rand(n, 1);
+
+% Reference
+
+u_r = [delta_ss; omega_F_ss; omega_R_ss];
+zeta_r = [V_ss; beta_ss; dpsidt_ss];
+
+% Simulation
+
+for k = 1:tf/ts
+    % U(:, k) = u_r - K*(Zeta([1, 2, 4], k) - zeta_r);
+    U(:, k) = rhc(A_d, B_d, Q, R, zeta_r, u_r, Zeta([1, 2, 4], k));
+    [t, zeta] = ode45(@(t, zeta) dynamics(zeta, U(:, k), p), [(k - 1)*ts k*ts], Zeta(:, k));
     Zeta(:, k + 1) = zeta(end, :);  
 end
 
+% Figures
+
 figure(1)
-plot(Zeta(6, :), Zeta(7, :))
+plot(Zeta(5, :), Zeta(6, :))
 
 figure(2)
 plot(Zeta(4, :))
@@ -57,4 +78,4 @@ plot(Zeta(4, :))
 figure(3)
 plot(U(1, :))
 
-figures(Zeta(6, :)', Zeta(7, :)', Zeta(3, :)', p, tf, h)  
+figures(Zeta(5, :)', Zeta(6, :)', Zeta(3, :)', p, tf, ts)  
