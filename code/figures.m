806f5faa2e850d5f7e4c1b39eeaf861c1c0a2606
Jodok Dittmann <jodok@dittmann.at>
2025-03-17 12:01:18 +0100

    Changed visualization and moved MPC to main

--- code/figures.m
+++ code/figures.m
@@ -1,22 +1,39 @@
-function figures(x, y, psi, p, tf, h)
+function figures(Zeta, U, p, tf, h)
 
-    figure(4); clf; hold on;
+    clf;
+    
+    figure(1)
+    plot(Zeta(5, :), Zeta(6, :))
 
-    for i = 1:1000:tf/h + 1
+    figure(2)
+    plot(Zeta(4, :))
 
-        car = [- p.l_F*cos(psi(i)) + p.w/2*sin(psi(i)) + x(i), p.l_R*cos(psi(i)) + p.w/2*sin(psi(i)) + x(i), p.l_R*cos(psi(i)) - p.w/2*sin(psi(i)) + x(i), - p.l_F*cos(psi(i)) - p.w/2*sin(psi(i)) + x(i);
-              - p.l_F*sin(psi(i)) - p.w/2*cos(psi(i)) + y(i), p.l_R*sin(psi(i)) - p.w/2*cos(psi(i)) + y(i), p.l_R*sin(psi(i)) + p.w/2*cos(psi(i)) + y(i), - p.l_F*sin(psi(i)) + p.w/2*cos(psi(i)) + y(i)];
+    figure(3)
+    plot(U(1, :))
 
-        plot([car(1, :), car(1, 1)], [car(2, :), car(2, 1)], 'b-', 'LineWidth', 2);
+    figure(4)
+    for i = 1:500:tf/h + 1
 
-        quiver(x(i), y(i), cos(psi(i)), sin(psi(i)), 1.5, 'r', 'LineWidth', 2);
-
-        pause(0.1);
+        plot(Zeta(5, :), Zeta(6, :), 'g--');
+        hold on; 
+  
+        rear = [Zeta(5, i) - p.l_R*cos(Zeta(3, i)), Zeta(6, i) - p.l_R*sin(Zeta(3, i))];
+        front = [Zeta(5, i) + p.l_F*cos(Zeta(3, i)), Zeta(6, i) + p.l_F*sin(Zeta(3, i))];
+        line([rear(1) front(1)], [rear(2) front(2)], 'Color', 'b', 'LineWidth', 2);
+  
+        d = 0.6; 
+        reartire = [rear(1) - d/2*cos(Zeta(3, i)), rear(1) + d/2*cos(Zeta(3, i)), rear(2) - d/2*sin(Zeta(3, i)), rear(2) + d/2*sin(Zeta(3, i))];
+        line([reartire(1) reartire(2)], [reartire(3) reartire(4)], 'Color', 'k', 'LineWidth', 8);
+  
+        fronttire = [front(1) - d/2*cos(Zeta(3, i) + U(1, i)), front(1) + d/2*cos(Zeta(3, i) + U(1, i)), front(2) - d/2*sin(Zeta(3, i) + U(1, i)), front(2) + d/2*sin(Zeta(3, i) + U(1, i))];
+        line([fronttire(1) fronttire(2)],[fronttire(3) fronttire(4)], 'Color','k','LineWidth',8);
+  
     end
 
     axis equal;
-    xlim([min(x)-5, max(x)+5]);
-    ylim([min(y)-5, max(y)+5]);
+    xlim([min(Zeta(5, :)) - 5, max(Zeta(5, :)) + 5]);
+    ylim([min(Zeta(6, :)) - 5, max(Zeta(6, :)) + 5]);
     grid on;
+
 end
 
--- code/main.m
+++ code/main.m
@@ -8,8 +8,8 @@ dpsidt_ss = V_ss/R_ss;
 
 % Equilibria
 
-x0 = [V_ss, beta_ss, 0, dpsidt_ss, 0, -40.7*pi/180, 0, 20.44, 58.33];
-[delta_ss, omega_F_ss, omega_R_ss] = equilibria(V_ss, beta_ss, dpsidt_ss, p, x0);
+zeta_0 = [V_ss, beta_ss, 0, dpsidt_ss, 0, -40.7*pi/180, 0, 20.44, 58.33];
+[delta_ss, omega_F_ss, omega_R_ss] = equilibria(V_ss, beta_ss, dpsidt_ss, p, zeta_0);
 
 m = 3;
 n = 6;
@@ -30,52 +30,73 @@ B_c = double(subs(B_c, {zeta1, zeta2, zeta4, u1, u2, u3}, {V_ss, beta_ss, dpsidt
 % Discretisation
 
 ts = 1e-3;
-tf = 30;
+tf = 10;
 
 [A_d, B_d] = c2d(A_c, B_c, ts);
-
-% LQR
-
-l = 3;
-
-Q = eye(l);
-R = eye(m);
-
-K = dlqr(A_d, B_d, Q, R);
-
-% MPC
-
-N = 10;
-
-% Initialisation
-
-U = nan(m, tf/ts + 1); 
-Zeta = nan(n, tf/ts + 1); 
-Zeta(:, 1) = [V_ss; beta_ss; 0; dpsidt_ss; 0; 0] + rand(n, 1);
 
 % Reference
 
 u_r = [delta_ss; omega_F_ss; omega_R_ss];
 zeta_r = [V_ss; beta_ss; dpsidt_ss];
 
+% MPC
+
+l = 3;
+
+Q = 2*eye(l);
+R = eye(m);
+
+N = 15;
+
+u_min = [-Inf; -Inf; -Inf];
+u_max = [Inf; Inf; Inf];
+zeta_min = [-Inf; -Inf; -Inf];
+zeta_max = [Inf; Inf; Inf];
+
+P = 10*Q;
+
+zeta_0 = zeta_r + 2*rand(3, 1);
+   
+H = blkdiag( kron(speye(N), Q), P, kron(speye(N), R) );
+f = [repmat(-Q*zeta_r, N, 1); -P*zeta_r; repmat(-R*u_r, N, 1)];
+   
+G_eq = [kron(speye(N+1), -speye(l)) + kron(sparse(diag(ones(N, 1), -1)), A_d), kron([sparse(1, N); speye(N)], B_d)];
+g_eq = [-zeta_0; zeros(N*l, 1)];
+
+u_eq = g_eq;
+    
+G_ineq = speye((N+1)*l + N*m);  
+g_ineq = [repmat(zeta_min, N+1, 1); repmat(u_min, N, 1)];
+
+u_ineq = [repmat(zeta_max, N+1, 1); repmat(u_max, N, 1)];
+    
+G_c = [G_eq; G_ineq];
+g_c = [g_eq; g_ineq];
+
+u_c = [u_eq; u_ineq];
+
+prob = osqp;
+
+prob.setup(H, f, G_c, g_c, u_c, 'verbose', 0);
+ 
+% Initialisation
+
+U = nan(m, tf/ts + 1); 
+Zeta = nan(n, tf/ts + 1); 
+Zeta(:, 1) = [zeta_0(1); zeta_0(2); rand; zeta_0(3); rand; rand];
+
 % Simulation
 
 for k = 1:tf/ts
-    % U(:, k) = u_r - K*(Zeta([1, 2, 4], k) - zeta_r);
-    U(:, k) = rhc(A_d, B_d, Q, R, zeta_r, u_r, Zeta([1, 2, 4], k));
+    sol = prob.solve();
+    U(:, k) = sol.x((N + 1)*l + 1:(N + 1)*l + m);
     [t, zeta] = ode45(@(t, zeta) dynamics(zeta, U(:, k), p), [(k - 1)*ts k*ts], Zeta(:, k));
-    Zeta(:, k + 1) = zeta(end, :);  
+    Zeta(:, k + 1) = zeta(end, :);
+    g_c(1:l) = -zeta(end, [1, 2, 4]);
+    u_c(1:l) = -zeta(end, [1, 2, 4]);
+    prob.update('l', g_c, 'u', u_c);
 end
 
 % Figures
 
-figure(1)
-plot(Zeta(5, :), Zeta(6, :))
-
-figure(2)
-plot(Zeta(4, :))
-
-figure(3)
-plot(U(1, :))
-
-figures(Zeta(5, :)', Zeta(6, :)', Zeta(3, :)', p, tf, ts)  
+figures(Zeta, U, p, tf, ts)  
